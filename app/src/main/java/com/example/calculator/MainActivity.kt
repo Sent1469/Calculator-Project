c120dba4fd285a213e48cb7276d3c1ff1a59fce3
Sent1469 <pronscnr6519@uwec.edu>
2021-10-06 22:05:45 -0500

    Got all the main functions running.

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+var result = "0"
+var numberTwo = "0"
+var firstNum = true
+var mathFunc = ""
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn1 = findViewById<Button>(R.id.button1)
+        val btn2 = findViewById<Button>(R.id.button2)
+        val btn3 = findViewById<Button>(R.id.button3)
+        val btn4 = findViewById<Button>(R.id.button4)
+        val btn5 = findViewById<Button>(R.id.button5)
+        val btn6 = findViewById<Button>(R.id.button6)
+        val btn7 = findViewById<Button>(R.id.button7)
+        val btn8 = findViewById<Button>(R.id.button8)
+        val btn9 = findViewById<Button>(R.id.button9)
+        val addBtn = findViewById<Button>(R.id.addBtn)
+        val subBtn = findViewById<Button>(R.id.subBtn)
+        val multBtn = findViewById<Button>(R.id.multBtn)
+        val divBtn = findViewById<Button>(R.id.divBtn)
+        val decBtn = findViewById<Button>(R.id.decimalBtn)
+        val equalsBtn = findViewById<Button>(R.id.equalsBtn)
+        val clearBtn = findViewById<Button>(R.id.clearBtn)
+        var calcText = findViewById<TextView>(R.id.textView)
+
+        val intButtons = arrayOf(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9)
+        val ariButtons = arrayOf(addBtn, subBtn, multBtn, divBtn)
+        val operators = arrayOf("+", "-", "*", "/")
+        // Creates all the number and arithmetic buttons and sets up even listeners for them
+        for(i in 0..8) {
+            intButtons[i].setOnClickListener {
+                calcText.append((i+1).toString())
+                firstOrSecond((i+1).toString())
+            }
+        }
+        for(i in 0..3) {
+            ariButtons[i].setOnClickListener {
+                setOperator(operators[i])
+                calcText.append(operators[i])
+            }
+        }
+        equalsBtn.setOnClickListener {
+            result = evaluate(mathFunc, result, numberTwo)
+            numberTwo = "0"
+            mathFunc = ""
+            calcText.text = result
+        }
+        clearBtn.setOnClickListener {
+            calcText.text = ""
+            mathFunc = ""
+            result = "0"
+            numberTwo = "0"
+            firstNum = true
+        }
+    }
+    // Handles evaluating expressions based off of the mathFunc that has been selected
+    private fun evaluate(func: String, first: String, second: String) : String {
+        var num = 0.0
+        if(func == "+") {
+            num = first.toDouble() + second.toDouble()
+        } else if(func == "-") {
+            num = first.toDouble() - second.toDouble()
+        } else if(func == "*") {
+            num = first.toDouble() * second.toDouble()
+        } else if(func == "/") {
+            num = first.toDouble() / second.toDouble()
+        } else {
+            return first
+        }
+        return num.toString()
+    }
+    // Determines weather it is the first or second number being inputted
+    private fun firstOrSecond(num: String) {
+        if(firstNum) {
+            result += num
+        } else {
+            numberTwo += num
+        }
+    }
+    private fun setOperator(operator: String) {
+        mathFunc = operator
+        firstNum = false
+    }
+}
